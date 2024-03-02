c455bd2ed389c7bb57ecc25d4a8e0fdf95760db8
JiebinHu <458249864@qq.com>
2024-03-02 11:43:16 +0800

    feat：启动的时候获取一下openid（前端）

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,60 @@
 // index.js
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
-Component({
-  data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
+Page({
+    data: {
+        motto: 'Hello MiniProgram',
+        userInfo: {
+            avatarUrl: defaultAvatarUrl,
+            nickName: '',
+        },
+        hasUserInfo: false,
+        canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+        canIUseNicknameComp: wx.canIUse('input.type.nickname'),
     },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
-    imageIconList: [
-      'https://tdesign.gtimg.com/miniprogram/images/icon1.png',
-      'https://tdesign.gtimg.com/miniprogram/images/icon2.png',
-    ],
-  },
-  methods: {
     // 事件处理函数
-    bindViewTap() {
-      console.log('bindViewTag')
+    onLoad() {
+        console.log('onLoad')
+        wx.login({
+            success(res) {
+                if (res.code) {
+                    console.log("res:" + JSON.stringify(res))
+                } else {
+                    console.log("登录失败")
+                }
+            }
+        })
     },
-    onChooseAvatar(e) {
-      const { avatarUrl } = e.detail
-      const { nickName } = this.data.userInfo
-      this.setData({
-        "userInfo.avatarUrl": avatarUrl,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
+    methods: {
+
+        onChooseAvatar(e) {
+            const {avatarUrl} = e.detail
+            const {nickName} = this.data.userInfo
+            this.setData({
+                "userInfo.avatarUrl": avatarUrl,
+                hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+            })
+        },
+        onInputChange(e) {
+            const nickName = e.detail.value
+            const {avatarUrl} = this.data.userInfo
+            this.setData({
+                "userInfo.nickName": nickName,
+                hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+            })
+        },
+        getUserProfile(e) {
+            // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+            wx.getUserProfile({
+                desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+                success: (res) => {
+                    console.log(res)
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        },
     },
-    onInputChange(e) {
-      const nickName = e.detail.value
-      const { avatarUrl } = this.data.userInfo
-      this.setData({
-        "userInfo.nickName": nickName,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    getUserProfile(e) {
-      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-      wx.getUserProfile({
-        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-          console.log(res)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    },
-  },
 })
